145433f2d03285adbe144074a8b956cb92ebc6e9
Vegard Bønes <vegardb@met.no>
2025-07-03 08:34:53 +0000

    add starting code for reading checkpoint

--- bris_workflow/payload/graph.py
+++ bris_workflow/payload/graph.py
@@ -1,5 +1,17 @@
 from earthkit.workflows.decorators import as_payload
+import torch
+
+_CHECKPOINT_FILE = './cloudy-skies.ckpt'
 
 @as_payload
 def create():
-    return 'graph'
+    return _CHECKPOINT_FILE
+    # return _create()
+
+def _create():
+    ckpt = torch.load(_CHECKPOINT_FILE, weights_only=False, map_location=torch.device('cpu'))
+    return ckpt
+
+if __name__ == "__main__":
+    ckpt = _create()
+    print(ckpt.__class__)

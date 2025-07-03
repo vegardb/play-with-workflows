82e0936746af49878a38f539e523a932ab508a30
Vegard Bønes <vegardb@met.no>
2025-07-03 08:32:42 +0000

    restructure

--- bris_workflow/execute.py
+++ bris_workflow/execute.py
@@ -11,24 +11,19 @@ import time
 
 def make_json(action: fluent.Action) -> str:
     '''Convert a workflow action to a JSON string for submission.'''
-    request, _ = _get_submit_request(action)
+    request, _ = get_submit_request(action)
     return request.model_dump_json(indent=2, warnings='error')
 
 
 def submit_json(raw_json: str, cascade_address: str, wait: bool=True) -> JobId:
     request = api.SubmitJobRequest.model_validate_json(raw_json)
-    return _submit_job(request, cascade_address, wait)
+    job_id = submit_job(request, cascade_address)
+    if wait:
+        await_job(job_id, cascade_address)
+    return job_id
 
 
-def action(action: fluent.Action, cascade_address: str = "tcp://localhost:8067"):
-    '''Throwaway function to execute a workflow action and return the result.'''
-
-    request, sinks = _get_submit_request(action)
-    job_id = _submit_job(request, cascade_address)
-    return _get_job_results(job_id, sinks, cascade_address)
-
-
-def _get_submit_request(action: fluent.Action) -> typing.Tuple[api.SubmitJobRequest, set[DatasetId]]:
+def get_submit_request(action: fluent.Action) -> typing.Tuple[api.SubmitJobRequest, set[DatasetId]]:
     graph = Cascade(action.graph())
     job = graph2job(graph._graph)
     sinks = cascade_views.sinks(job)
@@ -44,7 +39,7 @@ def _get_submit_request(action: fluent.Action) -> typing.Tuple[api.SubmitJobRequ
     return api.SubmitJobRequest(job=job), sinks
 
 
-def _submit_job(request: api.SubmitJobRequest, cascade_address: str, wait=True) -> JobId:
+def submit_job(request: api.SubmitJobRequest, cascade_address: str) -> JobId:
     submit_job_response: api.SubmitJobResponse = client.request_response(
         request, cascade_address)  # type: ignore
     if submit_job_response.error is not None:
@@ -54,28 +49,38 @@ def _submit_job(request: api.SubmitJobRequest, cascade_address: str, wait=True) 
         raise RuntimeError("Job submission did not return a job ID")
 
     job_id = submit_job_response.job_id
+    return job_id
 
-    if not wait:
-        return job_id
-    
+
+def await_job(job_id: JobId, cascade_address: str):
+    """Wait for a job to complete."""
     available_request = api.JobProgressRequest(job_ids=[job_id])
     done = False
     while not done:
         time.sleep(1)
-        availability: api.JobProgressResponse = client.request_response(
+        resp: api.JobProgressResponse = client.request_response(
             available_request, cascade_address)  # type: ignore
 
-        if availability.error is not None:
-            raise RuntimeError(f'Job failed: {availability.error}')
+        status = resp.progresses[job_id]
 
-        done = availability.progresses[job_id].pct == '100.00'
+        # if resp.error is not None:
+        #     raise RuntimeError(f'Job failed: {resp.error}')
 
-    return job_id
+        if status.failure:
+            print() # Add a newline, bacause of the complete \r trick, below
+            raise RuntimeError(f'Job {job_id} failed: {status.failure}')
+        
+        print(f'\r{status.pct}% complete', end='')
 
+        done = status.pct == '100.00'
+    print()
 
-def _get_job_results(job_id: JobId, sinks: set[DatasetId], cascade_address: str) -> api.ResultRetrievalResponse:
-    result_request = api.ResultRetrievalRequest(
-        job_id=job_id, dataset_id=list(sinks)[0])
-    response: api.ResultRetrievalResponse = client.request_response(
-        result_request, cascade_address)  # type: ignore
-    return api.decoded_result(response, job=None)  # type: ignore
+def get_job_results(job_id: JobId, sinks: set[DatasetId], cascade_address: str) -> typing.Dict[DatasetId, api.ResultRetrievalResponse]:
+    ret = {}
+    for s in sinks:
+        result_request = api.ResultRetrievalRequest(
+            job_id=job_id, dataset_id=s)
+        response: api.ResultRetrievalResponse = client.request_response(
+            result_request, cascade_address)  # type: ignore
+        ret[s] = api.decoded_result(response, job=None)  # type: ignore
+    return ret
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import click
 import bris_workflow.action as action
 import bris_workflow.execute as execute
-from datetime import datetime, UTC
+from datetime import datetime
 
 @click.group()
 def cli():
@@ -11,11 +11,26 @@ def cli():
 @cli.command()
 @click.option('--time', type=click.DateTime(), default=datetime(2025,7,1, 0, 0, 0), help='Time to get data for')
 @click.option('--cascade', default='tcp://localhost:8067', help='Address of the Cascade server')
-def run(time: datetime, cascade: str):
+@click.option('--no-wait', is_flag=True, help='Do not wait for the job to complete')
+def run(time: datetime, cascade: str, no_wait: bool):
     '''Run a workflow, gathering and preparing data for a bris run.'''
     a = action.create(time=time)
-    results = execute.action(a, cascade_address=cascade)
-    print(results)
+    request, sinks = execute.get_submit_request(a)
+    job_id = execute.submit_job(request, cascade)
+    print(f"Job submitted with ID: {job_id}.")
+
+    if no_wait:
+        return
+
+    try:
+        execute.await_job(job_id, cascade)
+    except Exception as e:
+        print(e)
+        return
+    results = execute.get_job_results(job_id, sinks, cascade)
+
+    for r in results:
+        print(r, results[r])
 
 @cli.group()
 def json():
@@ -36,23 +51,6 @@ def submit(cascade: str, no_wait: bool):
     '''Read json from stdin and submit it to cascade.'''
     execute.submit_json(click.get_text_stream('stdin').read(), cascade, not no_wait)
 
-
-def locally():
-    import cascade.benchmarks.job1 as j1
-    import cascade.benchmarks.distributed as di
-    import cloudpickle
-
-    spec = di.ZmqClusterSpec.local(j1.get_prob())
-    print(spec.controller.outputs)
-    # prints out:
-    # {DatasetId(task='mean:dc9d90 ...
-    # defaults to all "sinks", but can be overridden
-
-    rv = di.launch_from_specs(spec, None)
-
-    for key, value in rv.outputs.items():
-        deser = cloudpickle.loads(value)
-        print(f"output {key} is of type {type(deser)}")
 
 
 if __name__ == "__main__":
